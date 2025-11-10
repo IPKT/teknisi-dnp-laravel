b9227d11a448440a42e862e59dce38c449a5c0f4
ipkt <putu.kembar@gmail.com>
2025-11-10 14:37:05 +0800

    poo

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,36 +9,36 @@
             <div class="row g-3">
                 <div class="col-md-3">
                     <div class="card text-bg-primary">
-                        <div class="card-body">
-                            <h6>Total Peralatan</h6>
+                        <div class="card-body p-3">
+                            <h5>Total Peralatan</h5>
                             <h3>{{ $seluruh_peralatan['jumlah'] }}</h3>
-                            <p>ON : {{ $seluruh_peralatan['on'] }} | OFF : {{ $seluruh_peralatan['off'] }}</p>
+                            <p class="my-1">ON : {{ $seluruh_peralatan['on'] }} | OFF : {{ $seluruh_peralatan['off'] }}</p>
                         </div>
                     </div>
                 </div>
                 @foreach ($peralatan as $jenis => $value)
                     <div class="col-md-3">
                         <div class="card text-bg-info">
-                            <div class="card-body">
-                                <h6>{{ $jenis }}</h6>
+                            <div class="card-body p-3">
+                                <h5>{{ $jenis }}</h5>
                                 <h3>{{ $value['jumlah'] }}</h3>
-                                <p>ON : {{ $value['on'] }} | OFF : {{ $value['off'] }}</p>
+                                <p class="my-1">ON : {{ $value['on'] }} | OFF : {{ $value['off'] }}</p>
                             </div>
                         </div>
                     </div>
                 @endforeach
                 <div class="col-md-3">
                     <div class="card text-bg-warning">
-                        <div class="card-body">
-                            <h6>Belum Dikunjungi Tahun {{ Carbon\Carbon::now()->year }}</h6>
+                        <div class="card-body p-3">
+                            <h5>Belum Dikunjungi Tahun {{ Carbon\Carbon::now()->year }}</h5>
                             <h3>{{ $belumDikunjungiTahunIni }}</h3>
-                            <p style="visibility: hidden;">ON : 0 | OFF : 0</p>
+                            <p class="my-1" style="visibility: hidden;">ON : 0 | OFF : 0</p>
                         </div>
                     </div>
                 </div>
             </div>
             <hr>
-            <h4>Pemeliharaan</h4>
+            <h4>Pemeliharaan {{ Carbon\Carbon::now()->year }}</h4>
             <div class="row g-3">
                 <!-- Grafik di kiri -->
                 <div class="col-md-6">
@@ -54,33 +54,33 @@
                 <div class="col-md-6">
                     <div class="row g-3">
                         <div class="col-md-6">
-                            <div class="card text-bg-primary h-100">
+                            <div class="card text-bg-primary ">
                                 <div class="card-body">
-                                    <h6>Total Pemeliharaan {{ Carbon\Carbon::now()->year }}</h6>
+                                    <h5>Total Pemeliharaan </h5>
                                     <h3>{{ $pemeliharaan['total'] }}</h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="card text-bg-info h-100">
+                            <div class="card text-bg-info ">
                                 <div class="card-body">
-                                    <h6>Site dikunjungi</h6>
+                                    <h5>Site dikunjungi</h5>
                                     <h3>{{ $pemeliharaan['site_dikunjungi'] }} atau {{ $pemeliharaan['persen'] }}%</h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="card text-bg-info h-100">
+                            <div class="card text-bg-info">
                                 <div class="card-body">
-                                    <h6>Pemeliharaan Bulan {{ Carbon\Carbon::now()->isoFormat('MMMM') }}</h6>
+                                    <h5>Pemeliharaan Bulan {{ Carbon\Carbon::now()->isoFormat('MMMM') }}</h5>
                                     <h3>{{ $pemeliharaan['bulan_ini'] }}</h3>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="card text-bg-info h-100">
+                            <div class="card text-bg-info ">
                                 <div class="card-body">
-                                    <h6>Pemeliharaan Bulan {{ Carbon\Carbon::now()->subMonth()->isoFormat('MMMM') }}</h6>
+                                    <h5>Pemeliharaan Bulan {{ Carbon\Carbon::now()->subMonth()->isoFormat('MMMM') }}</h5>
                                     <h3>{{ $pemeliharaan['bulan_lalu'] }}</h3>
                                 </div>
                             </div>
